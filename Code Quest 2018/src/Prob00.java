040c85dc0e19e69e7ce8bbd5d0191739895af05d
Chuyeon Lee <wonup2@hotmail.com>
2020-02-13 19:07:09 -0800

    .

--- Code Quest 2018/src/Prob00.java
+++ Code Quest 2018/src/Prob00.java
@@ -7,14 +7,14 @@ public class Prob00{
 	
 	public static void main(String[] args)throws IOException{
 		Scanner in = new Scanner(new File(file+".in.txt"));
-		PrintWriter out = new PrintWriter(new File("out.txt"));
+		
 		
 		int n = in.nextInt(); 
 		for(int i=0; i<n; i++){
-			//processing and output
+			
 		}
 		
-		in.close();out.close();
-		Check.check("out.txt", file+".out.txt");
+		in.close();
+	
 	}
 }
