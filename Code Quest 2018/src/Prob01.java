b5fe75baaac4cf5c1d38d0decd2432b4dbded515
Chuyeon Lee <wonup2@hotmail.com>
2020-02-13 19:06:53 -0800

    .

---
+++ Code Quest 2018/src/Prob01.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Prob01 {
+
+	static String file = "Prob01";
+	
+	public static void main(String[] args)throws IOException{
+		Scanner in = new Scanner(new File(file+".in.txt"));
+		PrintWriter out = new PrintWriter(new File("out.txt"));
+		
+		
+		
+		int n = in.nextInt(); in.nextLine();
+		for(int i=0; i<n; i++){
+			if(in.nextInt()>=80) out.println("PASS");
+			else out.println("FAIL");
+		}
+				
+		
+		in.close();out.close();
+		Check.check("out.txt", file+".out.txt");
+	}
+}
---
+++ Code Quest 2018/src/Prob03.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Prob03 {
+
+	static String file = "Prob03";
+	
+	public static void main(String[] args)throws IOException{
+		Scanner in = new Scanner(new File(file+".in.txt"));
+		PrintWriter out = new PrintWriter(new File("out.txt"));
+		
+		int n = in.nextInt(); 
+		String result = "";
+		for(int i=0; i<n; i++){
+			String num = in.next().replace("th", "");
+			int last=num.length()-1;
+			
+			if(num.length()>1 && num.charAt(last-1)=='1') result += num+"th\n";	 			 
+			else {
+				if(num.charAt(last)=='1') result += num+"st\n";
+				else if(num.charAt(last)=='2')	result += num+"nd\n";
+				else if(num.charAt(last)=='3')	result += num+"rd\n";
+				else result += num+"th\n";
+			}
+		}		
+		
+		out.print(result);
+		in.close();out.close();
+		Check.check("out.txt", file+".out.txt");
+	}
+}
